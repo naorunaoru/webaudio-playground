5179b6838179de2590295597cdb47f6922ec8c38
Roman Kuraev <naoru.desu@gmail.com>
2026-01-05 13:54:56 +0000

    feat: gh pages deploy?

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-});
+function computeBase(): string {
+  if (process.env.VITE_BASE) return process.env.VITE_BASE;
 
+  const githubRepo = process.env.GITHUB_REPOSITORY; // "owner/repo"
+  const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+  if (isGithubActions && githubRepo) {
+    const repoName = githubRepo.split("/")[1];
+    if (repoName) {
+      if (repoName.endsWith(".github.io")) return "/";
+      return `/${repoName}/`;
+    }
+  }
+
+  return "/";
+}
+
+export default defineConfig(() => ({
+  base: computeBase(),
+  plugins: [react()],
+}));
